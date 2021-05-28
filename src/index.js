3d510840a7e534ce776f481be8563c3dd1725f74
hunman89 <hunman89@gmail.com>
2021-05-28 23:05:45 +0900

    add. apollo client

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./components/App";
+import client from "./apollo";
+import { ApolloProvider } from "@apollo/react-hooks";
 
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
 
   document.getElementById("root")
 );
