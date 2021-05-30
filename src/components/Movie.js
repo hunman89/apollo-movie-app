461030252bfdba0c450858292ffabd2b89edc93b
hunman89 <hunman89@gmail.com>
2021-05-30 22:53:42 +0900

    add. get Movie Query

---
+++ src/components/Movie.js
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default ({ id }) => (
+  <div>
+    <Link to={`/${id}`}>{id}</Link>
+  </div>
+);
